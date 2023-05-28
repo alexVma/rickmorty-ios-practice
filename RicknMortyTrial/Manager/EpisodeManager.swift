4d2866e6b1ae18353931b2051baaf4df6493b7d2
alex vladimir  minga arias <alexminga.a@gmail.com>
2023-05-28 14:26:02 -0500

    local change Sun May 28 14:26:02 -05 2023

--- RicknMortyTrial/Manager/EpisodeManager.swift
+++ RicknMortyTrial/Manager/EpisodeManager.swift
@@ -34,4 +34,4 @@
                    print("something is wrong")
                 }
             }.resume()
-println(Sat May 27 14:26:15 -05 2023) }
+println(Sun May 28 14:26:02 -05 2023) }
--- RicknMortyTrial/Manager/LocationManager.swift
+++ RicknMortyTrial/Manager/LocationManager.swift
@@ -30,4 +30,4 @@ func downloadLocationJSON(){
             print("something is wrong")
         }
     }.resume()
-println(Sat May 27 14:26:15 -05 2023) }
+println(Sun May 28 14:26:02 -05 2023) }
